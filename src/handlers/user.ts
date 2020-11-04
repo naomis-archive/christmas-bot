e66190019708ad251ee4f058aeaa4f049e28adee
nhcarrigan <nhcarrigan@gmail.com>
2020-11-03 17:21:20 -0800

    Initial database structure
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,1 +1,15 @@
-//TODO: Build user handler
+import { Document, model, Schema } from "mongoose";
+
+export interface UserInt extends Document {
+  userId: string;
+  username: string;
+  inventory: string[];
+}
+
+const user = new Schema({
+  userId: String,
+  username: String,
+  inventory: Array,
+});
+
+export const userModel = model<UserInt>("user", user);
