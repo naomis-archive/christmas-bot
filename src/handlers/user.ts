cf548f6b1e334f02d95b905a1ecd39824d32dc95
rgroves <rwgdev@gmail.com>
2020-11-10 17:49:47 -0600

    Add logic to track user inventory progress.
    Signed-off-by: rgroves <rwgdev@gmail.com>

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,15 +1,53 @@
-import { Document, model, Schema } from "mongoose";
+import { userModel } from "../data/userSchema";
 
-export interface UserInt extends Document {
-  userId: string;
-  username: string;
-  inventory: string[];
-}
+/**
+ * Adds an item to a user's inventory. This method will also create the user if
+ * it does not exist.
+ * @param {string} id - The Discord id that will be used to keep track of the user.
+ * @param {string} name - The user name belonging to the Discord user.
+ * @param {string} item - The item to add to the user's inventory.
+ * @return {boolean} true if the item added was not previously in the user's
+ * inventory; otherwise false.
+ */
+const addInventoryItem = async (
+  id: string,
+  name: string,
+  item: string
+): Promise<boolean> => {
+  try {
+    // Find an existing user (or create one) and add the item to the user's
+    // inventory. This call returns the user as it existed before the update.
+    const found = await userModel.findOneAndUpdate(
+      {
+        // Find user with this Discord User ID
+        userId: id,
+      },
+      {
+        // If upserting a new user, set the username.
+        $setOnInsert: {
+          username: name,
+        },
+        // Add the item to the user's inventory.
+        $addToSet: { inventory: item },
+      },
+      {
+        upsert: true,
+      }
+    );
 
-const user = new Schema({
-  userId: String,
-  username: String,
-  inventory: Array,
-});
+    // Check if the item was newly added to the user's inventory. Since the user
+    // returned above is in the state prior to the update, if the item exists in
+    // the inventory this user already had the item. If the user was upserted
+    // above, the found user will be null, but this also means the item was
+    // newly added.
+    const itemAdded = !found?.inventory.includes(item);
+    return Promise.resolve(itemAdded);
+  } catch (err) {
+    console.log(err);
+    return Promise.resolve(false);
+  }
+};
 
-export const userModel = model<UserInt>("user", user);
+export const user = {
+  addInventoryItem,
+};
