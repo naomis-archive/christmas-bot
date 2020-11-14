23a6f9f357f3dc94279ca235db09ff0c435f34ca
nhcarrigan <nhcarrigan@gmail.com>
2020-11-14 12:47:22 -0800

    Remove the role from other users
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- src/handlers/winner.ts
+++ src/handlers/winner.ts
@@ -23,12 +23,22 @@ export const handleWinner = async (message: Message) => {
     .sort({ uniqueItems: -1, totalItems: -1 })
     .limit(1);
   const currentWinnerUser = currentWinner[0];
-  const targetUser = await message.guild?.members.cache.find(
+  const targetUser = message.guild?.members.cache.find(
     (member) => member.id === currentWinnerUser.userId
   );
   if (!targetUser) {
     console.error("Target user not found.");
     return;
   }
+  const allUsers = await message.guild?.members.fetch();
+  if (!allUsers) {
+    console.error("User list error");
+    return;
+  }
+  allUsers.forEach(async (member) => {
+    if (member.roles.cache.get(winnerRole.id)) {
+      await member.roles.remove(winnerRole);
+    }
+  });
   await targetUser.roles.add(winnerRole);
 };
